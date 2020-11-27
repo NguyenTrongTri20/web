39f1e2ff2fefc5603ac418c979ad6ee475fbd24f
unknown <trongtri.ntt2000@gmail.com>
2020-11-27 14:55:21 +0700

    fix date time

--- web-master/TourNest-master/assets/js/custom.js
+++ web-master/TourNest-master/assets/js/custom.js
@@ -90,7 +90,7 @@ $(document).ready(function(){
         
         function makeTimer() {
 
-                var endTime = new Date("March 7, 2018 12:00:00 PDT");            
+                var endTime = new Date("August 20, 2021 12:00:00 PDT");            
                 var endTime = (Date.parse(endTime)) / 1000;
 
                 var now = new Date();
